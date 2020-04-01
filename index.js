76f153fa744c6df1b8c95d4609d9369a91b031fe
Danny Ferdinandusz <dannyferdinandusz@dannys-imac.home>
2020-04-01 11:36:40 +1100

    api/genres

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
-const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
-
 
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
@@ -11,12 +9,6 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-
-
-
-
-
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
--- models/customer.js
+++ models/customer.js
@@ -1,8 +1,6 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-
-
 const Customer = mongoose.model('Customer', new mongoose.Schema({
   name: {
     type: String,
@@ -22,16 +20,15 @@ const Customer = mongoose.model('Customer', new mongoose.Schema({
   }
 }));
 
-
 function validateCustomer(customer) {
   const schema = {
-    name:Joi.string().min(5).max(50).required(),
-    phone:Joi.string().min(5).max(50).required(),
+    name: Joi.string().min(5).max(50).required(),
+    phone: Joi.string().min(5).max(50).required(),
     isGold: Joi.boolean()
   };
 
   return Joi.validate(customer, schema);
 }
 
-exports.Customer = Customer ;
+exports.Customer = Customer; 
 exports.validate = validateCustomer;
